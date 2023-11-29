716dc72e9807da5747892a921387f0fd9ea049e0
EneroneJR <63048615+EneroneJR@users.noreply.github.com>
2023-11-29 22:50:33 +0100

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,64 +1,102 @@
-use std::io;
-use std::io::Write;
-// use rand::Rng; qui non serve
+/*enum IpAddr
+{
+    V4,
+    V6,
+}*/
 
-// "use" ci permette di avvisare cosa stiamo importando e di che libreria
-// std::io, stiamo importando la libreria di input/output
-// rand::Rng, stiamo importando la libreria di random
+enum IpAddr
+{
+    //V4(String),
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
 
-// ATTENZIONE! LA LIBRERIA rand::Rng è una libreria CHE METTIAMO TRAMITE IL CARGO.TOML!!!
+// possiamo anche sfruttare un enumeratore per tenere più strutture dati o fare di più!
 
-fn main() // main è il nome del programma ed fn è "funzione"
-{ //gli argomenti non sono passati. se voglio usarli, uso: std::env::args()
+enum Message
+{
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+// Possiamo anche inserirci metodi!
+impl Message
+{
+    fn funzione()
+    {
+        println!("Questo è un esempio");
+    }
+}
 
-    //print!("Inserisci un numero: "); 
-    //sarebbe più corretto usare print, ma ciò provoca l'attesa di un Input, prima di fare il print.
-    // per fare un normale print dobbiamo specificare un pacchetto di std::io... std::io::Write
-    // di conseguenza il codice basilare corretto è:
-    print!("Inserisci un numero: ");
-    io::stdout().flush().unwrap(); //unwrap è quasi un equivalente di .expect(), si può usare infatti al suo posto. flush invece è come readline ecc.
+struct IpAddress
+{
+    kind: IpAddr,
+    address: String,
+}
 
-    let mut guess = String::new(); 
-    // let è la dichiarazione di una variabile che non può essere modificata. DEFINISCE UNA VARIABILE
-    // mut rende la variabile modificabile
-    // String è la dichiarazione
-    // new è una funzione che crea una nuova stringa vuota. SOLO STRINGA
-    // La combo String::new(), è una stringa a capienza variabile.
-    // inoltre :: indica che il new è associata al tipo String
-    // altrimenti la variabile avrà il tipo assegnato dal compilatore in automatico.
-    // altrimenti usa: "let mut guess: String = String::new();"
+fn main() 
+{
+    /* Esempio poco pratico di cosa potrebbe fare un Enumeratore
+    let quattro = IpAddr::V4;
+    let sei = ipAddr::V6;
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    // read_line è una funzione che legge una riga di input da tastiera
+    let localhost = IpAddress
+    {
+        kind: IpAddr::V4,
+        address: String::from("127.0.0.1"),
+    };
+    */ // Sistemiamo
 
-    // si può scrivere anche:
-    /*
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");    
-    */
+    // let localhost = IpAddr::V4(String::from("127.0.0.1"));
 
-    // io.stdin() ritorna un istanza di std::io::Stdin
-    // .read_line(&mut guess) ritorna un istanza di std::io::Result< || Invoca il metodo che legge una riga di input da tastiera>
-    // &mut guess ritorna un istanza di std::io::Result<String>. Crea un errore perchè il compilatore vuole sapere dove và l'input che è pure modificabile.
-    // .expect("Failed to read line") ritorna un istanza di std::io::Result. Gestisce, seppur male, un errore o problemi durante l'uso d stdin
+    let localhost = IpAddr::V4(127, 0, 0, 1);
 
-    // !!!RUST PASSA PER RIFERIMENTO!!!
+    // in Rust non esiste il valore NULL, ma possiamo fare "l'option enum", Enum opzionale:
+/*    enum Option<T>
+    {
+        Some(T),
+        None,
+    }
+*/ // Questa cosa è così importante che Rust lo ha implentato già da sè!
 
-    println!("Hai inserito: {}", guess);
-    // il messaggio in uscita è come c: metti un "blank" {} e poi inserisci la variabile
+    let qualche_numero =Some(5);
+    let qualche_stringa = Some("Una stringa");
+
+    let numero_assente: Option<i32> = None; // nota come nel caso di Some, non abbiamo dovuto implementare il tipo!!!
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
+    // let sum: <i8 as Add<Option<i8>>>::Outpot = x + y; è un errore perchè i tipi sono diversi, quindi dobbiamo estrarne la variabile.
+    // però e specifico per l'option usata
+    let sum = x + y.unwrap_or(0);
+
 
 }
 
-// di più su .expect("...") :
+fn route(ip_kind: IpAddress)
+{
+}
 
-/*
-    | std::io::Stdin::readl_line ritorna std::result::Result, per la precisione std::io::Result<usize>
-    | in Rust non esistono eccezioni. I risultati (KO o OK) devono essere esplicitamente gestiti.
-    | se ci dimentichiamo di gestirli, il programma non compila o il compilatore ci dice di farlo.
-    | Result<T, E> è un tipo di dati che rappresenta un risultato di una operazione. Un enumerazione:
-    | T è il risultato OK! quindi andata bene / Ok(T)
-    | E è il risultato KO! quindi andata negativa/ Err(E)
-    | .expect("...") è una funzione che restituisce il risultato di una operazione. In questo caso ritorna il numero di caratteri letti
-    nell'esempio il valore viene ignorato, l'operazione fallisce e fa esplodere il programma (panic) con il messaggio indicato "failed to read line".
-*/
+enum Coin
+{
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn valore_in_centesimi(coin: Coin) -> u8
+{
+    match coin
+    {
+        Coin::Penny => {
+            println!("Penny fortunato!");
+            50;
+        },  //esempio
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
